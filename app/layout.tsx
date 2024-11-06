fcfc2ea53e9d9698e9af60c3120bc876e626a57d
Vitor Hugo <vitorhmartins@outlook.com.br>
2024-11-06 19:45:56 -0300

    feat: add auth with clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -29,7 +30,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}
       >
-        <ClerkProvider>{children}</ClerkProvider>
+        <ClerkProvider
+          appearance={{
+            baseTheme: dark,
+          }}
+        >
+          {children}
+        </ClerkProvider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,17 @@
 import { UserButton } from "@clerk/nextjs";
-//import { auth } from "@clerk/nextjs/server";
-//import { redirect } from "next/navigation";
-//import { Button } from "./_components/ui/button";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 const HomePage = async () => {
-  /*const { userId } = await auth();
+  const { userId } = await auth();
   if (!userId) {
     redirect("/login");
-  }*/
+  }
   return (
     <div className="h-full flex items-center justify-center">
       <UserButton showName />
     </div>
   );
-
-  //<Button>Hello World</Button>
 };
 
 export default HomePage;

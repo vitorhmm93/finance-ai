5e7de1af669f7e345818c247d8d97f6333b0391f
Vitor Hugo <vitorhmartins@outlook.com.br>
2024-11-06 17:30:33 -0300

    feat: add userlogin with google

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
-import { Button } from "./_components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+//import { auth } from "@clerk/nextjs/server";
+//import { redirect } from "next/navigation";
+//import { Button } from "./_components/ui/button";
 
-const HomePage = () => {
-  return <Button>Hello World</Button>;
+const HomePage = async () => {
+  /*const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }*/
+  return (
+    <div className="h-full flex items-center justify-center">
+      <UserButton showName />
+    </div>
+  );
+
+  //<Button>Hello World</Button>
 };
 
 export default HomePage;

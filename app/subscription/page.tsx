7af73eb0da51356c9849ee188888e12daccff2bb
Vitor Hugo <vitorhmartins@outlook.com.br>
2024-11-08 12:04:37 -0300

    feat: ensure transaction and subscription pages

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,6 +1,14 @@
+import { redirect } from "next/navigation";
 import Navbar from "../_components/navbar";
+import { auth } from "@clerk/nextjs/server";
 
-const SubscriptionPage = () => {
+const SubscriptionPage = async () => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    redirect("/login");
+  }
+
   return (
     <>
       <Navbar />
--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -3,11 +3,22 @@ import { DataTable } from "../_components/ui/data-table";
 import { transactionColumns } from "./_columns";
 import AddTransactionButton from "../_components/add-transaction-button";
 import Navbar from "../_components/navbar";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 const TransactionsPage = async () => {
-  // acessar as transações do meu banco de dados
+  const { userId } = await auth();
 
-  const transactions = await db.transaction.findMany({});
+  if (!userId) {
+    redirect("/login");
+  }
+
+  // acessar as transações do meu banco de dados
+  const transactions = await db.transaction.findMany({
+    where: {
+      userId,
+    },
+  });
   return (
     <>
       <Navbar />
